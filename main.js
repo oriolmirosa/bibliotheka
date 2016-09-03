593eb0b9d3c13826531327d3a31e38abe9eab90e
Oriol Mirosa <mirosa@uwm.edu>
2016-09-03 15:03:20 -0500

    Added simple routing with react-router

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, browserHistory } from 'react-router'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path='/' component={App} />
+  </Router>
+  , document.getElementById('app'))
