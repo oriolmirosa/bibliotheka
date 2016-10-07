0fc0e46c1303e509aed7f189a339ebd51900f2e5
Oriol Mirosa <oriolmirosa@gmail.com>
2016-10-07 16:30:52 -0500

    Added selectOpen method to references that captures when a reference is clicked and what reference the click came from

--- src/components/views/ListItem.jsx
+++ src/components/views/ListItem.jsx
@@ -3,11 +3,17 @@ import React from 'react'
 class ListItem extends React.Component {
 	constructor (props) {
 		super(props)
+
+		this.selectOpen = this.selectOpen.bind(this)
+	}
+
+	selectOpen () {
+		console.log(`selectOpen called with ${JSON.stringify(this.props.id, null, 4)}`)
 	}
 
 	render () {
 		return (
-			<div style={{width: 100 + '%', height: 75}} onClick={this.props.whenClicked}>
+			<div style={{width: 100 + '%', height: 75}} onClick={this.selectOpen}>
 				<span>{this.props.author}.</span>&nbsp;
 				<span>{this.props.year}.</span>&nbsp;
 				<span>{this.props.title}.</span>&nbsp;
--- src/components/views/ListView.jsx
+++ src/components/views/ListView.jsx
@@ -6,17 +6,17 @@ class ListView extends Component {
 	constructor (props) {
 		super(props)
 
-		this.selectedOpen = this.selectedOpen.bind(this)
+		// this.selectedOpen = this.selectedOpen.bind(this)
 	}
 
-	selectedOpen () {
-		console.log(`selectedOpen called`)
-	}
+	// selectedOpen (id) {
+	// 	console.log(`selectedOpen called with ${JSON.stringify(id, null, 4)}`)
+	// }
 
   render () {
     const listItems = References.map((reference, index) => {
       return (
-				<ListItem author={reference.author} year={reference.year} title={reference.title} publication={reference.publication} details={reference.details} key={index} whenClicked={this.selectedOpen} />
+				<ListItem author={reference.author} year={reference.year} title={reference.title} publication={reference.publication} details={reference.details} pdf={reference.pdf} key={index} id={index} />
       )
     })
     return (
